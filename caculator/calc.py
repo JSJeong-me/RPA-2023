fa449815baba2cffd6d81115b3ffeba1b15dcac2
Joon Soo Jeong / Ph.D <jsjeong@hansung.ac.kr>
2023-03-21 22:24:41 +0900

    calculator

---
+++ caculator/calc.py
@@ -0,0 +1,19 @@
+from RPA.Desktop.Windows import Windows
+
+win = Windows()
+
+def open_calculator():
+    win.open_from_search("calc.exe", "계산기")
+    elements = win.get_window_elements()
+
+def make_calculations(expression):
+    win.send_keys(expression)
+    result = win.get_element_rich_text('id:CalculatorResults')
+    return int(result.strip('표시는 '))
+
+if __name__ == "__main__":
+    open_calculator()
+    exp = '5*2='
+    result = make_calculations(exp)
+    print(f"Calculation result of '{exp}' is '{result}'")
+    win.close_all_applications()
